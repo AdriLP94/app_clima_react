accdcba4f2ebe2e585314681939b4e2e9fc6c704
AdriLP94 <adrianlopezpalmero@gmail.com>
2019-09-25 18:06:25 +0200

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <nav>
+            <div className="nav-wrapper light-blue darken-2">
+                <span className="brand-logo">¿Qué tiempo hace?</span>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
