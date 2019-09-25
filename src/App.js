5cb6cb818d3a4af1e7fbe358d2c925846a3c8f70
AdriLP94 <adrianlopezpalmero@gmail.com>
2019-09-25 18:06:00 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import Error from'./components/Error';
 
 function App() {
+
+  const [ciudad, guardarCiudad] = useState('');
+  const [pais, guardarPais] = useState('');
+  const [error, guardarError] = useState(false)
+
+  const datosConsulta = datos => {
+    if(datos.ciudad === "" || datos.pais === "") {
+      guardarError(true);
+      return;
+    }
+    
+    guardarCiudad(datos.ciudad);
+    guardarPais(datos.pais);
+    guardarError(false);
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header/>
+        <div className="contenedor-form">
+          <div className="container">
+            <div className="row">
+              <div className="col s12 m6">
+                <Formulario
+                  datosConsulta = {datosConsulta}
+                />
+              </div>
+              <div className="col s12 m6">
+                {error ? <Error/> : null}
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
