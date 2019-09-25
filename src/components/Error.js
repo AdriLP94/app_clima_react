99c1ce4f407d2c9406b7498e54f5c0f369dec6c1
AdriLP94 <adrianlopezpalmero@gmail.com>
2019-09-25 18:06:16 +0200

    Create Error.js

---
+++ src/components/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div className ="card-panel red darken-4 error col s12">
+            Ambos campos son obligatorios
+        </div>
+    );
+};
+
+export default Error;
